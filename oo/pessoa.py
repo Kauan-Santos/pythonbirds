dec246379e88d0344f92c59b1c6cedda54967c7c
Kauan-Santos <kasantos.business@gmail.com>
2022-06-02 17:05:43 -0300

    Crisod atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,14 @@
 class Pessoa():
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá {self.nome}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Kauan', 22)
     print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    print(f'Nome: {p.nome}')
+    print(f'Idade: {p.idade}')
