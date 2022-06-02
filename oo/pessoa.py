69e21a12ddc7138331f913b8fb4269dca5645b7d
Kauan-Santos <kasantos.business@gmail.com>
2022-06-02 17:44:31 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,19 @@
 class Pessoa():
-    def __init__(self, nome=None, idade=None):
-        self.nome = nome
+    def __init__(self, *filhos, nome_completo=None, idade=None):
+        self.nome_completo = nome_completo
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {self.nome}'
+        return f'Olá {self.nome_completo}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Kauan', 22)
-    print(Pessoa.cumprimentar(p))
-    print(f'Nome: {p.nome}')
-    print(f'Idade: {p.idade}')
+    Kauan = Pessoa(nome_completo='Altieres Kauan dos Santos')
+    Gessica = Pessoa(nome_completo='Gessica Dos Santos')
+    Jose = Pessoa(Kauan, Gessica, nome_completo='José Vantuil dos Santos', idade=55)
+    print(Pessoa.cumprimentar(Jose))
+    print(f'Nome: {Jose.nome_completo}')
+    print(f'Idade: {Jose.idade}')
+    for filho in Jose.filhos:
+        print(f'Filhos: {filho.nome_completo}')
